33e744723db5165f8a15e55d7bdc788f451eaa2a
yxlunar <2855104545@qq.com>
2025-06-11 13:53:12 +0800

    delete

--- Assets/AttackSystem/Scripts/Else/Tag.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Tag
-{
-    public static string PICKUPABLIE = "PickupAble";
-    public static string CLIMBABLE = "ClimbAble";
-}
